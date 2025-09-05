8b299c705e6bfddb3f2294614f1c31fa2449c971
rasikasrimal <77890327+rasikasrimal@users.noreply.github.com>
2025-09-05 16:57:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 
 # ============================
-# Heart Disease Risk App + CSV Upload/EDA/Batch Predict
+# Heartlytics + CSV Upload/EDA/Batch Predict
 # ============================
 
 import os
