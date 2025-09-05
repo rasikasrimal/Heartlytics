940a41380d8995cbbc1f62792cc236b63d388334
Rasika Srimal <77890327+rasikasrimal@users.noreply.github.com>
2025-09-05 18:27:45 +0530

    Delay simulations until form submit and highlight current point

--- tests/test_simulations.py
+++ tests/test_simulations.py
@@ -1,9 +1,14 @@
 """Tests for simulations page."""
 
 def test_simulations_page(auth_client):
-    resp = auth_client.get("/simulations/?variable=age")
+    resp = auth_client.get("/simulations/")
     assert resp.status_code == 200
-    assert b'exercise-angina-chart' in resp.data
-    assert b'Heart disease risk change with Age' in resp.data
-    assert b'What-If' not in resp.data
-    assert b'Age Projection' not in resp.data
+    assert b"exercise-angina-chart" not in resp.data
+
+    resp = auth_client.post("/simulations/", data={"variable": "age"})
+    assert resp.status_code == 200
+    assert b"exercise-angina-chart" in resp.data
+    assert b"Heart disease risk change with Age" in resp.data
+    assert b"You\\'re here" in resp.data
+    assert b"What-If" not in resp.data
+    assert b"Age Projection" not in resp.data
